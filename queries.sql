75f9bae88e4d05eb46842fbaf5872fb7df94ba9f
Oumaima Nejjari <oumanj1998@gmail.com>
2023-06-15 20:01:21 +0100

    Add new tables to data file

--- queries.sql
+++ queries.sql
@@ -55,3 +55,4 @@ SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 
 -- The average number of escape attempts per animal type of those born between 1990 and 2000
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
