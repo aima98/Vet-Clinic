bdb6edd9a2bda443cea58cd2b6f3014dc3ed5497
Oumaima Nejjari <oumanj1998@gmail.com>
2023-06-15 16:25:56 +0100

    Add screenshots

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 CREATE TABLE animals(
     id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name TEXT NOT NULL,
     date_of_birth DATE,
     escape_attempts INT,
     neutered BOOLEAN,
